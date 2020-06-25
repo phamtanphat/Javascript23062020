87b09c203aaa42b575e26082c64c52a758b6d584
phamtanphat <phatdroid94@gmail.com>
2020-06-25 06:56:23 -0700

    object vs array

--- buoi1.js
+++ buoi1.js
@@ -12,3 +12,21 @@
 // Th1 : Khai bao 1 bien khong gan gia tri 
 // var a
 // console.log(a)
+// TH2 : Truy van toi key khong ton tai cua object
+// const teo = {
+//     name : "Nguyen Van Teo",
+//     age : 10
+// }
+// console.log(teo.address)
+
+// 3 : Object
+// const teo = {
+//     name : "Nguyen Van Teo",
+//     age : 10
+// }
+// console.log(teo.address)
+
+// 4 : Array
+const arrayNames = ["Teo","Ti","Tun"]
+arrayNames[0] = "Tuan"
+console.log(arrayNames[0])
