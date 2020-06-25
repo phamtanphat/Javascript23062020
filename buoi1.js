d133384993d9831f94a0c3925af2caaa23edf1bd
phamtanphat <phatdroid94@gmail.com>
2020-06-25 07:34:10 -0700

    buoi1

--- buoi1.js
+++ buoi1.js
@@ -18,6 +18,12 @@
 //     age : 10
 // }
 // console.log(teo.address)
+// Th3 : Function khong return
+// function showName(name){
+//     console.log(name)
+//     return
+// }
+// console.log(showName("Phat"))
 
 // 3 : Object
 // const teo = {
@@ -34,14 +40,13 @@
 
 // 5 : Toan tu
 // + - * / % , ++ , --
-
 // Do uu tien cua toan tu
 // 1 :  ++ --
 // 2 :  * , /
 // 3 :  + , -
-var a = 5
-var b = 6 
-var ketqua = a++ - --b  + --a + b-- + ++a - b++ + b-- - b++
+// var a = 5
+// var b = 6 
+// var ketqua = a++ - --b  + --a + b-- + ++a - b++ + b-- - b++
 // 5 - --b  + --a + b-- + ++a - b++ + b-- - b++  a = 6 , b = 6
 // 5 - 5  + --a + b-- + ++a - b++ + b-- - b++  a = 6 , b = 5
 // 5 - 5  + 5 + b-- + ++a - b++ + b-- - b++  a = 5 , b = 5
@@ -51,7 +56,24 @@ var ketqua = a++ - --b  + --a + b-- + ++a - b++ + b-- - b++
 // 5 - 5  + 5 + 5 + 6 - 4 + 5 - b++  a = 6 , b = 4
 // 5 - 5  + 5 + 5 + 6 - 4 + 5 - 4  a = 6 , b = 5
 // 13
-
-console.log(ketqua)
-
+// console.log(ketqua)
 // ? (10 , 13 , 12)
+
+// 6 : Function
+// function showName(name){
+//     console.log(name)
+//     return
+// }
+// console.log(showName("Phat"))
+
+// 7 : Object method
+const teo = {
+    name : "Nguyen Van Teo",
+    age : 10,
+    info : function(){
+        console.log("Name " + this.name)
+        console.log("Age " + this.age)
+    }
+}
+teo.info()
+
