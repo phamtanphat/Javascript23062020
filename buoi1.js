da7e8c769597cfc58faf40189f29ce0f7b8375aa
phamtanphat <phatdroid94@gmail.com>
2020-06-25 06:27:18 -0700

    khai bao bien , kieu du lieu

---
+++ buoi1.js
@@ -0,0 +1,14 @@
+// 1 : Khai bao bien
+// var a = 5
+// const b = 6
+// a = 10
+// b = 12
+// console.log(a)
+// console.log(b)
+
+// 2 : Kieu du lieu
+// null 
+// undenfined
+// Th1 : Khai bao 1 bien khong gan gia tri 
+// var a
+// console.log(a)
