871091d14bd7d1b5d6e3bf4ca670f7dad23853d3
phamtanphat <phatdroid94@gmail.com>
2020-06-30 06:59:31 -0700

    array method

---
+++ buoi2.js
@@ -0,0 +1,31 @@
+// 11 : For of
+//Array
+// const arrayNames = ["Teo","Ti","Tun","Hoa","Tuan"]
+// for (const value of arrayNames) {
+//     console.log(value)
+// }
+//Object
+// const teo = {
+//     name : "Nguyen Van Teo",
+//     age : 10
+// }
+// for (const value of ) {
+//     console.log(value)
+// }
+
+// 12 : Array Method
+// const arrayNames = ["Teo","Ti","Tun"] 
+// const arrayNames2 = ["Hoa","Tuan"] 
+// Them vi tri cuoi
+// arrayNames.push("Toan")
+// Xoa vi tri cuoi
+// arrayNames.pop()
+// Them vi tri dau
+// arrayNames.unshift("A")
+// Xoa vi tri dau
+// arrayNames.shift()
+// Noi du lieu
+// const newArrays = arrayNames.concat(arrayNames2)
+// console.log(newArrays)
+
+
