56af4dc69fd0b7188c04e552b62719ac018876da
phamtanphat <phatdroid94@gmail.com>
2020-07-02 06:51:35 -0700

    bai tap reduce

--- buoi3.js
+++ buoi3.js
@@ -26,11 +26,67 @@
  * findDivisibleNum([1, 3, 4], 2) => 4
  */
 
-function findDivisibleNum(array, x) {
-    // viết code ở đây.
-    return array.find(function(value){
-        if(value % x == 0) return true
-        return false 
-    })
+// function findDivisibleNum(array, x) {
+//     // viết code ở đây.
+//     return array.find(function(value){
+//         if(value % x == 0) return true
+//         return false 
+//     })
+// }
+// console.log(findDivisibleNum([1, 3, 4], 2))
+
+// 17 : Reduce
+// const arrayNumbers = [1,2,3,4,5,6,7,8,9,10]
+
+// const total = arrayNumbers.reduce(function(accumulator , currentValue){
+//     console.log("Accumulator " + accumulator + " CurrentValue " + currentValue)
+//     return accumulator + currentValue 
+// },0)
+
+// Trả về số lượng người đã vote
+// function totalVotes(arr) {
+//     return arr.reduce(function(accum , currentvalue){
+//         if ( currentvalue.voted){
+//             accum++
+//         }
+//         return accum
+//     },0)
+// }
+
+// var voters = [
+//   {name:'Bob' , age: 30, voted: true},
+//   {name:'Jake' , age: 32, voted: true},
+//   {name:'Kate' , age: 25, voted: false},
+//   {name:'Sam' , age: 20, voted: false},
+//   {name:'Phil' , age: 21, voted: true},
+//   {name:'Ed' , age:55, voted:true},
+//   {name:'Tami' , age: 54, voted:true},
+//   {name: 'Mary', age: 31, voted: false},
+//   {name: 'Becky', age: 43, voted: false},
+//   {name: 'Joey', age: 41, voted: true},
+//   {name: 'Jeff', age: 30, voted: true},
+//   {name: 'Zack', age: 19, voted: false}
+// ];
+// console.log(totalVotes(voters)) // 7
+
+function countOccurrences(arr) {
+    return arr.reduce(function(accum , currentvalue){
+        if(currentvalue in accum){
+            accum[currentvalue]++
+        }else{
+            accum[currentvalue] = 1
+        }
+        return accum
+    },{})
 }
-console.log(findDivisibleNum([1, 3, 4], 2))
+console.log(countOccurrences(["a", "b", "c", "b", "a", "a"]))
+
+// {"a" : 3 , "b" : 2 , "c" :1 }
+
+// const teo = {
+//     name :"nguyen Van teo",
+//     age : 20
+// }
+
+// teo['address'] = "38 nguyen lam"
+// console.log(teo)
