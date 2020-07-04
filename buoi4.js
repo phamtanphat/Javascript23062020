5d2d9c4146ec0083abf75ee1d6700d33c34610f8
phamtanphat <phatdroid94@gmail.com>
2020-07-04 07:26:35 -0700

    apply

--- buoi4.js
+++ buoi4.js
@@ -60,15 +60,63 @@
 // }
 // teo.showName()
 
-const arrayNames = ['Teo','Ti','Tun']
+// const arrayNames = ['Teo','Ti','Tun']
 // const newArrayNames = arrayNames.map(function(value , index){
 //     return 'Ti'
 // })
-const newArrayNames = arrayNames.map(value => {
-    return value
-})
-console.log(newArrayNames)
+// const newArrayNames = arrayNames.map(value => {
+//     return value
+// })
+// console.log(newArrayNames)
 
-const showName = () => {
+// const showName = () => {
 
+// }
+
+// 27 : Template string
+
+// const teo = {
+//     name : "Nguyen Van Teo",
+//     age : 20
+// }
+// const string = "Xin chao " + teo.name + " . Ban hien tai " + teo.age + " tuoi"
+// const string1 = `Xin chao  ${teo.name}  . Ban hien tai ${teo.age}  tuoi`
+// console.log(string)
+// console.log(string1)
+
+
+//28 : argument
+
+// function tinhtong(){
+//     return Array.from(arguments).reduce((accum , currentValue) => {
+//         return accum + currentValue
+//     },0)
+// }
+// console.log(tinhtong(1,2,3,4,5,6,7,8,9,10))
+
+
+// 29 : Default param
+// function tinhtong(a = 5, b = 5){
+//     console.log(a + b)
+// }
+// tinhtong()
+
+//30 : Call
+
+// function greeting(name,age){
+//     console.log(`Hi! ${name} . I am ${age} . Address ${this.address}`)
+// }
+// greeting.call({address : 'Quan 10'} , 'Tom' , 10)
+
+//31 : Apply
+function sum(){
+    const numbers = Array.from(arguments)
+    return numbers.reduce((sum , num )=> sum + num , 0)
 }
+function average(){
+    const x = sum.apply(null,arguments)
+    return x / arguments.length
+}
+average(1,2,3,6)
+  
+  
