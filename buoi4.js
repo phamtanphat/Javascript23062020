53993389ebc75c06f281f884af62532bc6c5c678
phamtanphat <phatdroid94@gmail.com>
2020-07-04 06:54:21 -0700

    arrow function

--- buoi4.js
+++ buoi4.js
@@ -44,3 +44,31 @@
 // }
 // const showName2 = teo.showName.bind({name : "Nguyen van TI"})
 // showName2()
+
+// 26 : Arrow function
+
+// const teo = {
+//     name : 'Nguyen Van Teo',
+//     age : 20,
+//     showName : function(){
+//         console.log(this.name)
+//         var a = () => {
+//             console.log(this.name)
+//         }
+//         a()
+//     }
+// }
+// teo.showName()
+
+const arrayNames = ['Teo','Ti','Tun']
+// const newArrayNames = arrayNames.map(function(value , index){
+//     return 'Ti'
+// })
+const newArrayNames = arrayNames.map(value => {
+    return value
+})
+console.log(newArrayNames)
+
+const showName = () => {
+
+}
