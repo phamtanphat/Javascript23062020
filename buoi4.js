3fcb449f45a49a5f5da789ba9601f8babd30979e
phamtanphat <phatdroid94@gmail.com>
2020-07-04 06:18:44 -0700

    cach su dung tu khoa khai bao bien

---
+++ buoi4.js
@@ -0,0 +1,34 @@
+// 22 : Toan tu 3 ngoi
+// const a = 5
+// const b = 6
+
+// if(a > b){
+//     console.log("A lon hon b")
+// }else{
+//     console.log("A be hon hoac bang B")
+// }
+// console.log(a > b ? "A lon hon B" : "A be hon hoac bang B")
+
+// 23 : Function scope
+// var a = 5
+// function tinhtong(){
+//     var a = 7
+//     var b = 6
+//     return a + b;
+// }
+// console.log(tinhtong())
+
+// var a = 1
+// function random(){
+//   var a = Math.random()
+//   console.log(a)
+// }
+// random()
+// console.log(a)
+
+// 24 : Cach su dung tu khoa khai bao bien
+
+for(let i = 0 ; i <= 10 ; i++){
+    console.log("AAA")
+}
+console.log(i)
