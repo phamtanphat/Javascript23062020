3b2fa4dfcd745d9e4488b4051c1f7a9858b5bd2e
phamtanphat <phatdroid94@gmail.com>
2020-07-07 06:31:42 -0700

    destructuring

--- buoi5.js
+++ buoi5.js
@@ -5,9 +5,22 @@
 // console.log(sum(1,2,3,4,5))
 
 //33 : spread (coppy cac phan tu)
-const arrNums = [1,2,3]
+// const arrNums = [1,2,3]
 
-const newArr = arrNums
-newArr.pop()
-console.log(arrNums)
-console.log(newArr)
+// const newArr = arrNums
+// newArr.pop()
+// console.log(arrNums)
+// console.log(newArr)
+
+// 34 : Destructuring
+// const arrNums = [5,2,1,3,0,9,6]
+// const [a,b,c,d,e] = arrNums
+// console.log(b)
+
+const teo = {
+    name : 'teo',
+    age : 10,
+    address : 'Quan 10'
+}
+const {name , age , address } = teo
+console.log(name)
